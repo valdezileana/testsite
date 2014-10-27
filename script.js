705b4921930b9a1b7366fa0436b4674e5d575e62
Ashley <ashleywu2016@u.northwestern.edu>
2014-10-27 11:59:28 -0500

    responsive progress

--- script.js
+++ script.js
@@ -3,13 +3,7 @@ $(document).ready(function(){
 // Fancybox Function
 $(".fancybox").fancybox();
 
-$("#234").click(takeMeHome);
-
-function takeMeHome(){
-	console.log("You're home!");
-}
-
-
+// function changeNav
 
 });
 
