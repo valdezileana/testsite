8c9d23b8f214db8b99dc1fcd45db99a2c9ed74b3
Ashley <ashleywu2016@u.northwestern.edu>
2014-11-11 02:37:41 -0600

    javascript

--- script.js
+++ script.js
@@ -3,5 +3,16 @@ $(document).ready(function(){
 // Fancybox Function
 $(".fancybox").fancybox();
 
+Nav Bar action
+$(".menu-icons").hover(function(){
+	$(this).animate({height:"60px"}, "fast");
+	$(this).css("margin", "0 1.2% 0 1.2%");
+});
+
+$(".menu-icons").mouseleave(function(){
+    $(this).animate({height:"52px"}, "fast");
+    $(this).css("margin", "0 6% 0 6%");
+});
+
 });
 
