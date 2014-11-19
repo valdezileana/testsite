cc9dc553b1b5bf274dfdaacd2334ec9ffb195d37
Ashley <ashleywu2016@u.northwestern.edu>
2014-11-19 11:40:03 -0600

    hover on nav bar, delete work-photo

--- script.js
+++ script.js
@@ -3,16 +3,60 @@ $(document).ready(function(){
 // Fancybox Function
 $(".fancybox").fancybox();
 
-// Nav Bar action
-$(".menu-icons").hover(function(){
-	$(this).animate({height:"60px"}, "fast");
-	$(this).css("margin", "0 1.2% 0 1.2%");
-});
+//Turn Nav Bar icons black when hovered over, temp solution (there may be a faster way)
+	$("#home").hover(function(){
+		$("#home").attr("src", "Images/Nav/home-black.png");
+	});
 
-$(".menu-icons").mouseleave(function(){
-    $(this).animate({height:"52px"}, "fast");
-    $(this).css("margin", "0 6% 0 6%");
-});
+	$("#home").mouseleave(function(){
+		$("#home").attr("src", "Images/Nav/home-dark.png");
+	});
+
+	$("#work").hover(function(){
+		$("#work").attr("src", "Images/Nav/work-black.png");
+	});
+
+	$("#work").mouseleave(function(){
+		$("#work").attr("src", "Images/Nav/work-dark.png");
+	});
+
+	$("#about").hover(function(){
+		$("#about").attr("src", "Images/Nav/about-black.png");
+	});
+
+	$("#about").mouseleave(function(){
+		$("#about").attr("src", "Images/Nav/about.png");
+	});
+
+	$("#contact").hover(function(){
+		$("#contact").attr("src", "Images/Nav/contact-black.png");
+	});
+
+	$("#contact").mouseleave(function(){
+		$("#contact").attr("src", "Images/Nav/contact-dark.png");
+	});
+
+	$("#resume").hover(function(){
+		$("#resume").attr("src", "Images/Nav/resume-black.png");
+	});
+
+	$("#resume").mouseleave(function(){
+		$("#resume").attr("src", "Images/Nav/resume-dark.png");
+	});
+
+//How do I make the icon unclickable once you're on the page?
+
+
+//Turn social fontawesome icons gray when hovered over
+
+	// $(".fontawesome-").hover(function(){
+	// 	$(".fontawesome-:before").css("display", "none");
+	// 	$(this).css({"color","#939598"});
+	// });
+
+	// $("#social").mouseleave(function(){
+	// 	$(this).css("color","black");
+	// });
 
 });
 
