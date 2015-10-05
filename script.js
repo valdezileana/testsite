b2648568638a301ba23e20d4cbc46a4ff1bef681
Ashley <millenniumfalcon@Ashleys-MacBook-Pro-2.local>
2015-10-05 00:31:40 -0500

    some edits to the js

--- script.js
+++ script.js
@@ -1,5 +1,47 @@
 $(document).ready(function(){
 
+//Turn Nav Bar icons black when hovered over, temp solution (there may be a simpler way)
+$("#home").hover(function(){
+	$("#home").attr("src", "img/Nav/home-hover.png");
+});
+
+$("#home").mouseleave(function(){
+	$("#home").attr("src", "img/Nav/home.png");
+});
+
+$("#work").hover(function(){
+	$("#work").attr("src", "img/Nav/work-hover.png");
+});
+
+$("#work").mouseleave(function(){
+	$("#work").attr("src", "img/Nav/work.png");
+});
+
+$("#about").hover(function(){
+	$("#about").attr("src", "img/Nav/about-hover.png");
+});
+
+$("#about").mouseleave(function(){
+	$("#about").attr("src", "img/Nav/about.png");
+});
+
+$("#contact").hover(function(){
+	$("#contact").attr("src", "img/Nav/contact-hover.png");
+});
+
+$("#contact").mouseleave(function(){
+	$("#contact").attr("src", "img/Nav/contact.png");
+});
+
+$("#resume").hover(function(){
+	$("#resume").attr("src", "img/Nav/resume-hover.png");
+});
+
+$("#resume").mouseleave(function(){
+	$("#resume").attr("src", "img/Nav/resume.png");
+});
+
+
 // Fancybox Functions
 $("#single_1").fancybox()
     .attr('rel', 'gallery')
@@ -37,46 +79,6 @@ $("#ebola").fancybox()
         padding : 0
     });
 
-//Turn Nav Bar icons black when hovered over, temp solution (there may be a simpler way)
-	$("#home").hover(function(){
-		$("#home").attr("src", "img/Nav/home-hover.png");
-	});
-
-	$("#home").mouseleave(function(){
-		$("#home").attr("src", "img/Nav/home.png");
-	});
-
-	$("#work").hover(function(){
-		$("#work").attr("src", "img/Nav/work-hover.png");
-	});
-
-	$("#work").mouseleave(function(){
-		$("#work").attr("src", "img/Nav/work.png");
-	});
-
-	$("#about").hover(function(){
-		$("#about").attr("src", "img/Nav/about-hover.png");
-	});
-
-	$("#about").mouseleave(function(){
-		$("#about").attr("src", "img/Nav/about.png");
-	});
-
-	$("#contact").hover(function(){
-		$("#contact").attr("src", "img/Nav/contact-hover.png");
-	});
-
-	$("#contact").mouseleave(function(){
-		$("#contact").attr("src", "img/Nav/contact.png");
-	});
-
-	$("#resume").hover(function(){
-		$("#resume").attr("src", "img/Nav/resume-hover.png");
-	});
-
-	$("#resume").mouseleave(function(){
-		$("#resume").attr("src", "img/Nav/resume.png");
-	});
 
 });
 
